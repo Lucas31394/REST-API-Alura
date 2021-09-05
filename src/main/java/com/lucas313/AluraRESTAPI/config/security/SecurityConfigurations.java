7f120b5ec3e38ba221990d2ff1f19839b3e748a0
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-05 12:59:08 -0300

    Adicionando Spring Security ao projeto

---
+++ src/main/java/com/lucas313/AluraRESTAPI/config/security/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package com.lucas313.AluraRESTAPI.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+}
