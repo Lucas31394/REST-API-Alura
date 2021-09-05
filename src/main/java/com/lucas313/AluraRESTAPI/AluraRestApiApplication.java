581f799d7b30db3828ffb2c4a0257e7c89fc5401
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-05 12:18:41 -0300

    Adicionando valores padrões para páginação

--- src/main/java/com/lucas313/AluraRESTAPI/AluraRestApiApplication.java
+++ src/main/java/com/lucas313/AluraRESTAPI/AluraRestApiApplication.java
@@ -2,8 +2,10 @@ package com.lucas313.AluraRESTAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class AluraRestApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lucas313/AluraRESTAPI/controller/TopicoController.java
+++ src/main/java/com/lucas313/AluraRESTAPI/controller/TopicoController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,8 +43,7 @@ public class TopicoController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDTO> listaTopicos(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
+	public Page<TopicoDTO> listaTopicos(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao) {
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDTO.converter(topicos);
