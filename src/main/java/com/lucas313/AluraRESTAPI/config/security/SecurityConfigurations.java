390a8a801e268146f6ed791dacb0d0940e48d10e
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-06 14:05:00 -0300

    Beans baseados em profiles

--- src/main/java/com/lucas313/AluraRESTAPI/config/security/SecurityConfigurations.java
+++ src/main/java/com/lucas313/AluraRESTAPI/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package com.lucas313.AluraRESTAPI.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import com.lucas313.AluraRESTAPI.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
