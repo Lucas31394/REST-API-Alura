20980e1f5baaa08008a17ab9fc1c716f64d8811a
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-06 13:39:43 -0300

    Autorização baseada em Roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Okim0LeczrxnAW7xDwH/yOttfQT635JO7FAqgKO3eFjdug9XDHlMe');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$Okim0LeczrxnAW7xDwH/yOttfQT635JO7FAqgKO3eFjdug9XDHlMe');
+
+INSERT INTO PERFIL(id, nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
